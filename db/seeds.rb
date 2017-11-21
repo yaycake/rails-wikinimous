2f862cd36a32f9dfca6222f953db29496f56043c
Grace Yang <yaycake@gmail.com>
2017-11-21 14:52:33 +0800

    set up for Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+articles = Article.create(
+
+  10.times do |article|
+    Article.create([{ title: Faker::RickAndMorty.character }, {content: Faker::RickAndMorty.quote  })
+  end
+
